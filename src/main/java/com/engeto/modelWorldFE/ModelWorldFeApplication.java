f3c9501d12de07d216a925d7727a2b55db4cfe62
tomas_mokry <tomas.mokry@gmail.com>
2023-04-27 14:19:59 +0200

    init commit

---
+++ src/main/java/com/engeto/modelWorldFE/ModelWorldFeApplication.java
@@ -0,0 +1,13 @@
+package com.engeto.modelWorldFE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelWorldFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelWorldFeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/engeto/modelWorldFE/ModelWorldFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engeto.modelWorldFE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelWorldFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
